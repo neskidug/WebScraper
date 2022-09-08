11fce1f406923c93fbeb7e6570ffe28da9d4fbf6
RGUCN <10182474@ucn.dk>
2022-09-08 13:50:52 +0200

    Returning html

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,18 @@ namespace WebScraper
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
         {
+            HttpResponseMessage response = await client.GetAsync("https://www.timeanddate.no/vaer/?continent=europe&low=c");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Above three lines can be replaced with new helper method below
+            // string responseBody = await client.GetStringAsync(uri);
+
+            Console.WriteLine(responseBody);
+            Console.ReadLine(); 
+
         }
     }
 }
