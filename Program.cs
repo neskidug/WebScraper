613a4e6ceecaf6955df80db093589e9d1bf66b13
Rasmus <neskidug@gmail.com>
2022-11-01 10:09:20 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            HttpResponseMessage response = await client.GetAsync("https://www.timeanddate.no/vaer/?continent=europe&low=c");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Above three lines can be replaced with new helper method below
+            // string responseBody = await client.GetStringAsync(uri);
+
+            Console.WriteLine(responseBody);
+            Console.ReadLine(); 
+
+        }
+    }
+}
